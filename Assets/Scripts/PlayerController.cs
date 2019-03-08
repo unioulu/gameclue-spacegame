35641d2e178c6c8beb2d1b1607e8aeccb6781cc4
Johannes Kantola <johannes.kantola@student.oulu.fi>
2019-03-08 15:36:48 +0200

    Added horizontal movement, cleaned code

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -11,6 +11,8 @@ public class PlayerController : MonoBehaviour
     private Vector3 lastPos;
     private float boundary_left = -10f;
     private float boundary_right = 10f;
+    private float boundary_top = 4f;
+    private float boundary_bottom = -4f;
 
     // Start is called before the first frame update
     void Start()
@@ -21,8 +23,13 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.RightArrow) && (transform.position.x + speed) < boundary_right) { transform.position = new Vector3(lastPos.x + speed, transform.position.y, 0); }
-        if (Input.GetKey(KeyCode.LeftArrow) && (transform.position.x + speed) > boundary_left) { transform.position = new Vector3(lastPos.x - speed, transform.position.y, 0); }
+        Vector3 pos = transform.position;
+
+        if (Input.GetKey(KeyCode.RightArrow) && (pos.x + speed) < boundary_right) { transform.position = new Vector3(lastPos.x + speed, pos.y, 0); }
+        if (Input.GetKey(KeyCode.LeftArrow) && (pos.x + speed) > boundary_left) { transform.position = new Vector3(lastPos.x - speed, pos.y, 0); }
+        pos = transform.position;
+        if (Input.GetKey(KeyCode.UpArrow) && (pos.y + speed) < boundary_top) { transform.position = new Vector3(pos.x, lastPos.y + speed, 0); }
+        if (Input.GetKey(KeyCode.DownArrow) && (pos.y + speed) > boundary_bottom) { transform.position = new Vector3(pos.x, lastPos.y - speed, 0); }
         lastPos = transform.position;
 
         if (Input.GetKeyDown(KeyCode.Space)) 
