d53fb70b01cd85df717879bc6f66d9ea81473ced
Joni Rajala <jonimatiasrajala@gmail.com>
2019-03-18 13:20:05 +0200

    EventLogger and EventLog class created

---
+++ Assets/Scripts/EventLog.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class EventLog {
+
+    public string message { private set; get; }
+    public string timestamp { private set; get; }
+
+    public EventLog(string message)
+    {
+        this.message = message;
+        timestamp = Time.time.ToString();
+    }
+
+    override public string ToString()
+    {
+        return timestamp + " , " + message;
+    }
+
+}
---
+++ Assets/Scripts/EventLogger.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.IO;
+
+public class EventLogger
+{
+
+    private static EventLogger instance = null;
+
+    private List<EventLog> logs;
+
+    static EventLogger singleton()
+    {
+        if (instance == null)
+        {
+            instance = new EventLogger();
+        }
+        return instance;
+    }
+
+    private EventLogger()
+    {
+        logs = new List<EventLog>();
+    }
+
+
+    public void Log(string message)
+    {
+        AddLog(new EventLog(message));
+    }
+
+    public void AddLog(EventLog log)
+    {
+        logs.Add(log);
+    }
+
+    public void WriteToFile()
+    {
+        string desktopPath = System.Environment.GetFolderPath(System.Environment.SpecialFolder.Desktop);
+
+        using (StreamWriter file = new StreamWriter(desktopPath))
+        {
+            foreach (EventLog log in logs)
+            {
+                file.WriteLine(log.ToString());
+            }
+        }
+
+
+
+    }
+
+}
