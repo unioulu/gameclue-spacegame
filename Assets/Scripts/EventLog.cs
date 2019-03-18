01c99a2bc9d4aa7d76c41a5b6642c62654be6d03
Joni Rajala <jonimatiasrajala@gmail.com>
2019-03-18 14:15:52 +0200

    GameNameGenerator added to recognize games from one another during logging

--- Assets/Scripts/EventLog.cs
+++ Assets/Scripts/EventLog.cs
@@ -3,7 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.IO;
 
+
+
 public class EventLog {
+
+    public static class EventCode
+    {
+        static string KeyUp(KeyCode key) { return "KeyUp " + key; }
+        static string KeyDown(KeyCode key) { return "KeyDown " + key; }
+
+        const string damage = "PlayerDamage";
+        static string EnemyDamage(string enemyId) { return "EnemyDamage" + enemyId; }
+
+    }
 
     public string message { private set; get; }
     public string timestamp { private set; get; }
