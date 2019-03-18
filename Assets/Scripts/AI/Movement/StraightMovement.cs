4bebfc5ff76b0260449184ba60d83d8d68141e96
Jouni Pikkarainen <jouni.pikkarainen@gmail.com>
2019-03-18 13:22:01 +0200

    Fixes angle

--- Assets/Scripts/AI/Movement/StraightMovement.cs
+++ Assets/Scripts/AI/Movement/StraightMovement.cs
@@ -1,7 +1,33 @@
-﻿using System.Collections;using System.Collections.Generic;using UnityEngine;public class StraightMovement : MonoBehaviour{    [SerializeField]    private Rigidbody2D rb;    public float speed = 0.8f;    public float angle = 0f;    // Start is called before the first frame update    void Start() {        SetSpeedAndAngle(speed, angle);    }    // Update is called once per frame    void Update()    {            }    public void SetSpeedAndAngle(float speed, float angle)
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StraightMovement : MonoBehaviour
+{
+    [SerializeField]
+    private Rigidbody2D rb;
+
+    public float speed = 0.8f;
+    public float angle = 1.5f * Mathf.PI;
+
+    // Start is called before the first frame update
+    void Start() {
+        SetSpeedAndAngle(speed, angle);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        SetSpeedAndAngle(speed, angle);
+    }
+
+    public void SetSpeedAndAngle(float speed, float angle)
     {
         this.speed = speed;
         this.angle = angle;
-        Rigidbody2D rb = transform.GetComponent<Rigidbody2D>();
-        rb.velocity = new Vector3(angle, -speed, 0f);
-    }}
+
+        Vector3 velo = new Vector3(Mathf.Cos(angle), Mathf.Sin(angle), 0);
+        velo = velo * speed;
+        this.rb.velocity = velo;
+    }
+}
