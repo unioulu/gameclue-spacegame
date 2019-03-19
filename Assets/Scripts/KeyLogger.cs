a242e5b8b1f09e41428985fe85b010bdeffd42cc
Joni Rajala <jonimatiasrajala@gmail.com>
2019-03-19 11:13:53 +0200

    KeyLogger preloads its list of keys during Start

--- Assets/Scripts/KeyLogger.cs
+++ Assets/Scripts/KeyLogger.cs
@@ -4,26 +4,12 @@ using UnityEngine;
 
 public class KeyLogger : MonoBehaviour
 {
-
-    private List<KeyCode> codes;
+    private System.Array codes;
 
     // Start is called before the first frame update
     void Start()
     {
-        codes = new List<KeyCode>();
-        codes.Add(KeyCode.LeftArrow);
-        codes.Add(KeyCode.RightArrow);
-        codes.Add(KeyCode.UpArrow);
-        codes.Add(KeyCode.DownArrow);
-        codes.Add(KeyCode.J);
-        codes.Add(KeyCode.I);
-        codes.Add(KeyCode.K);
-        codes.Add(KeyCode.L);
-        codes.Add(KeyCode.Space);
-        codes.Add(KeyCode.W);
-        codes.Add(KeyCode.A);
-        codes.Add(KeyCode.S);
-        codes.Add(KeyCode.D);
+        codes = System.Enum.GetValues(typeof(KeyCode));
 
 
     }
@@ -31,25 +17,13 @@ public class KeyLogger : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        foreach (KeyCode code in System.Enum.GetValues(typeof(KeyCode))) {
-            if (Input.GetKeyDown(code))
-            {
-                EventLogger.Log(EventLog.EventCode.KeyDown(code));
-            }
-            if (Input.GetKeyUp(code))
-            {
-                EventLogger.Log(EventLog.EventCode.KeyUp(code));
-            }
-        }
-        /*foreach (KeyCode code in codes)
-        {
+        foreach (KeyCode code in codes) {
             if (Input.GetKeyDown(code)) {
                 EventLogger.Log(EventLog.EventCode.KeyDown(code));
             }
-            if (Input.GetKeyUp(code))
-            {
+            if (Input.GetKeyUp(code)) {
                 EventLogger.Log(EventLog.EventCode.KeyUp(code));
             }
-        }*/
+        }
     }
 }
