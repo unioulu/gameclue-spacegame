8bacfa24bf0e2077c1221fe64c671124a37f4c68
Johannes Kantola <johannes.kantola@student.oulu.fi>
2019-03-20 11:19:29 +0200

    Shooting enemies now aim towards player

--- Assets/Scripts/AI/EnemyShooting.cs
+++ Assets/Scripts/AI/EnemyShooting.cs
@@ -11,25 +11,36 @@ public class EnemyShooting : MonoBehaviour
 
     private GameObject player;
     private GameObject bulletInstance;
+    private Rigidbody2D rb;
 
-    private float shootTimer;
     private Vector3 playerPos;
+    private Vector3 targetPos;
+    private float shootTimer;
+    private float angle;
 
     // Start is called before the first frame update
     void Awake()
     {
         player = GameObject.FindWithTag("Player");
+        rb = gameObject.GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        playerPos = player.transform.position;
+        targetPos = (playerPos - transform.position).normalized;
+
+        if (transform.position.x < playerPos.x)
+            angle = Vector3.Angle(targetPos, Vector3.down);
+        else
+            angle = -Vector3.Angle(targetPos, Vector3.down);
+
+        rb.MoveRotation(angle);
         shootTimer += Time.deltaTime;
 
-        if(shootTimer > shootCooldown)
+        if (shootTimer > shootCooldown)
         {
-            playerPos = player.transform.position;
-            Vector3 targetPos = (playerPos - transform.position).normalized;
             bulletInstance = Instantiate(bulletPrefab, transform.position, Quaternion.identity);
             bulletInstance.GetComponent<Rigidbody2D>().velocity = targetPos * bulletSpeed;
             shootTimer = 0;
