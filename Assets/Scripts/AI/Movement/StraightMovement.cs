6052cef59013f1a79dc211f9017b7de4c0b0bb9e
piksu <jouni.pikkarainen@gmail.com>
2019-03-22 15:10:41 +0200

    Fixed StraightMovement format

--- Assets/Scripts/AI/Movement/StraightMovement.cs
+++ Assets/Scripts/AI/Movement/StraightMovement.cs
@@ -1,7 +1,26 @@
-﻿using System.Collections;using System.Collections.Generic;using UnityEngine;public class StraightMovement : MonoBehaviour{    [SerializeField]    private Rigidbody2D rb;    public float speed = 0.8f;    public float angle = 0f;    // Start is called before the first frame update    void Start() {        SetSpeedAndAngle(speed, angle);    }    // Update is called once per frame    void Update()    {            }    public void SetSpeedAndAngle(float speed, float angle)
-    {
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StraightMovement : MonoBehaviour {
+
+  [SerializeField]
+  private Rigidbody2D rb;
+  public float speed = 0.8f;
+  public float angle = 0f;
+
+  void Start() {
+    SetSpeedAndAngle(speed, angle);
+  }
+
+  void Update() {
+
+  }
+
+  public void SetSpeedAndAngle(float speed, float angle) {
         this.speed = speed;
         this.angle = angle;
         Rigidbody2D rb = transform.GetComponent<Rigidbody2D>();
         rb.velocity = new Vector3(angle, -speed, 0f);
-    }}
+  }
+}
