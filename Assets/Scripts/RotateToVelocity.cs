a3e1063a64ca762446483b2d4acb5523f92eb2c7
Joni Rajala <jonimatiasrajala@gmail.com>
2019-03-25 00:02:36 +0200

    Bullets rotate based on their velocity.

---
+++ Assets/Scripts/RotateToVelocity.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateToVelocity : MonoBehaviour
+{
+
+    public Rigidbody2D rb = null;
+
+    private float previousRotation = 0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0f, 0f, -previousRotation);
+
+        previousRotation = Vector3.SignedAngle(Vector3.up, rb.velocity, Vector3.forward);
+
+        Debug.Log(previousRotation);
+
+        transform.Rotate(0f, 0f, previousRotation);
+    }
+}
