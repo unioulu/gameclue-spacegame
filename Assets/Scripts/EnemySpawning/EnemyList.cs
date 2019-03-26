1fc0452f7d5c20320f2cfa27bd857a16f02559a1
Joni Rajala <jonimatiasrajala@gmail.com>
2019-03-26 10:24:27 +0200

    Placed files into appropriate folders

---
+++ Assets/Scripts/EnemySpawning/EnemyList.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyList
+{
+
+    public class EnemyData
+    {
+        public string id;
+        GameObject prefab;
+
+        public EnemyData(string id, GameObject prefab)
+        {
+
+        }
+
+        public void Construct()
+        {
+            GameObject.Instantiate(prefab);
+        }
+    }
+
+    private List<EnemyData> enemyList;
+
+    public void LoadDefaults()
+    {
+        var asteroid = new EnemyData("asteroid", Resources.Load();
+    }
+
+}
---
+++ Assets/Scripts/EnemySpawning/EnemySpawnList.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawnList
+{
+
+    private List<EnemySpawnEvent> spawnList;
+
+    public void LoadFromFile()
+    {
+
+    }
+
+    public void Generate()
+    {
+
+    }
+
+}
