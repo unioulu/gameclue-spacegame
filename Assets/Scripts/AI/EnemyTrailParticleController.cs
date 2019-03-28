008debb4ccfc6ea63328e627a33aabdd91d1f166
Joni Rajala <jonimatiasrajala@gmail.com>
2019-03-28 21:07:08 +0200

    Added EnemyTrailParticleController to EnemyTrailParticlePrefab that disables the particle emitter based on CueManager. Also added CueManager.

---
+++ Assets/Scripts/AI/EnemyTrailParticleController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyTrailParticleController : MonoBehaviour
+{
+
+    public ParticleSystem particles = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (CueManager.HasCues())
+        {
+            particles.Play();
+        }
+        else
+        {
+            particles.Stop();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/CueManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CueManager
+{
+
+    private static CueManager instance = null;
+
+
+    private bool hasCues = true;
+
+    static CueManager singleton()
+    {
+        if (instance == null)
+        {
+            instance = new CueManager();
+        }
+        return instance;
+    }
+
+    private CueManager()
+    {
+        
+    }
+
+
+    public static bool HasCues()
+    {
+        return singleton().hasCues;
+    }
+
+    public static void SetCues(bool tobe)
+    {
+        singleton().hasCues = tobe;
+    }
+
+}
