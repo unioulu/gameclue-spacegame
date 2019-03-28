af3a72e1d2f12f35034272b7cf7366d06654968a
Joni Rajala <jonimatiasrajala@gmail.com>
2019-03-28 20:07:03 +0200

    SineMovement has waveTime attribute, to manage wave length.

--- Assets/Scripts/AI/Movement/SinMovementEnemy.cs
+++ Assets/Scripts/AI/Movement/SinMovementEnemy.cs
@@ -7,6 +7,7 @@ public class SinMovementEnemy : MonoBehaviour
     public Vector3 direction = new Vector3(0,1f,0);
 
     public float amplitude = 3f;
+    public float waveTime = Mathf.PI;
 
     private Rigidbody2D rb = null;
     private float startXPos = 0f;
@@ -21,7 +22,7 @@ public class SinMovementEnemy : MonoBehaviour
 
     private void FixedUpdate()
     {
-        this.lifetime += Time.fixedDeltaTime;
+        this.lifetime += Time.fixedDeltaTime * (Mathf.PI / waveTime);
         float yPos = rb.position.y - Time.fixedDeltaTime * speed;
         float xPos = startXPos + Mathf.Sin(lifetime) * amplitude;
         rb.MovePosition(new Vector2(xPos, yPos));
